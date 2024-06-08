ad3b295d459c42c9fb0c2f1d8b08951a6475ddb0
DesKenn <150326372+DesKenn@users.noreply.github.com>
2024-06-08 12:26:33 -0400

    Update Objective9Lab5.java

--- Labs/Objective9Lab5.java
+++ Labs/Objective9Lab5.java
@@ -4,14 +4,83 @@
  */
 
 package com.mycompany.objective9lab5;
+import java.util.Scanner;
 
 /**
  *
  * @author dez
  */
 public class Objective9Lab5 {
+  public static void main(String[] args) {
+    Scanner kb = new Scanner(System.in);
+    double num1, num2;
+    boolean keepGoing = true;
+    int choice;
+    double answer = 0.0;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    System.out.print("Please enter the first number: ");
+    num1 = kb.nextDouble();
+
+    System.out.print("Please enter the second number: ");
+    num2 = kb.nextDouble();
+
+    while (keepGoing) {
+      printMenu();
+      System.out.print("Which would you like to do? ");
+      choice = kb.nextInt();
+
+      switch (choice) {
+
+        case 1:
+          answer = findSum(num1, num2);
+          System.out.println("The sum of " + num1 + " and " + num2 + " is: " + answer);
+          break;
+        case 2:
+          answer = findAverage(num1, num2);
+          System.out.println("The average of " + num1 + " and " + num2 + " is: " + answer);
+          break;
+        case 3:
+          answer = calcTax(num1, num2);
+          System.out.println("The amount of tax to be collected from " + num1 + " and " + num2 + " is: " + answer);
+          break;
+        case 4:
+          keepGoing = false;
+          System.out.println("Goodbye!");
+          break;
+        default:
+          System.out.println("Invalid choice. Please try again.");
+      }
     }
+    kb.close();
+  }
+
+  public static void printMenu() {
+    System.out.println();
+    System.out.println("========= MENU =========");
+    System.out.println("|                      |");
+    System.out.println("|   1. Add Numbers     |");
+    System.out.println("|   2. Find Average    |");
+    System.out.println("|   3. Calculate Tax   |");
+    System.out.println("|   4. Exit            |");
+    System.out.println("|                      |");
+    System.out.println("========================");
+    System.out.println();
+  }
+
+  public static double findSum(double x, double y) {
+    double sum = x + y;
+    return sum;
+  }
+
+ 
+  public static double findAverage(double x, double y) {
+    return (x + y) / 2;
+  }
+
+
+  public static double calcTax(double x, double y) {
+    double total = x + y;
+    double taxRate = 0.0831;
+    return total * taxRate;
+  }
 }
